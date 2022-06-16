2ffb315bbfb368943fe8c04111a26691c511a01c
parham-k <pkazemi3@gmail.com>
2022-06-16 11:16:12 -0700

    Add const to args getters

--- include/args.hpp
+++ include/args.hpp
@@ -42,26 +42,26 @@ class ProgramArguments
 
 	void parse(int argc, char** argv);
 
-	[[nodiscard]] unsigned get_num_threads() { return t; }
-	[[nodiscard]] unsigned get_kmer_length() { return k; }
-	[[nodiscard]] unsigned get_num_hashes() { return h; }
-	[[nodiscard]] unsigned get_hit_cap() { return hit_cap; }
-	[[nodiscard]] unsigned get_bits() { return bits; }
-	[[nodiscard]] unsigned get_bytes() { return bytes; }
-	[[nodiscard]] unsigned get_m() { return m; }
-	[[nodiscard]] unsigned get_dbf_size() { return dbf_size; }
-	[[nodiscard]] unsigned get_cbf_size() { return cbf_size; }
-	[[nodiscard]] unsigned get_hit_size() { return hit_size; }
-	[[nodiscard]] std::string get_prefix() { return prefix; }
-	[[nodiscard]] unsigned get_f0() { return F0; }
-	[[nodiscard]] unsigned get_f1() { return f1; }
-	[[nodiscard]] unsigned get_fr() { return fr; }
-	[[nodiscard]] bool out_bloom() { return _out_bloom; }
-	[[nodiscard]] bool solid() { return _solid; }
-	[[nodiscard]] bool long_mode() { return _long_mode; }
-	[[nodiscard]] bool use_ntcard() { return _use_ntcard; }
-	[[nodiscard]] std::vector<std::string> get_input_files() { return input_files; }
-	[[nodiscard]] std::vector<std::string> get_seeds() { return seeds; }
+	[[nodiscard]] unsigned get_num_threads() const { return t; }
+	[[nodiscard]] unsigned get_kmer_length() const { return k; }
+	[[nodiscard]] unsigned get_num_hashes() const { return h; }
+	[[nodiscard]] unsigned get_hit_cap() const { return hit_cap; }
+	[[nodiscard]] unsigned get_bits() const { return bits; }
+	[[nodiscard]] unsigned get_bytes() const { return bytes; }
+	[[nodiscard]] unsigned get_m() const { return m; }
+	[[nodiscard]] unsigned get_dbf_size() const { return dbf_size; }
+	[[nodiscard]] unsigned get_cbf_size() const { return cbf_size; }
+	[[nodiscard]] unsigned get_hit_size() const { return hit_size; }
+	[[nodiscard]] std::string get_prefix() const { return prefix; }
+	[[nodiscard]] unsigned get_f0() const { return F0; }
+	[[nodiscard]] unsigned get_f1() const { return f1; }
+	[[nodiscard]] unsigned get_fr() const { return fr; }
+	[[nodiscard]] bool out_bloom() const { return _out_bloom; }
+	[[nodiscard]] bool solid() const { return _solid; }
+	[[nodiscard]] bool long_mode() const { return _long_mode; }
+	[[nodiscard]] bool use_ntcard() const { return _use_ntcard; }
+	[[nodiscard]] std::vector<std::string> get_input_files() const { return input_files; }
+	[[nodiscard]] std::vector<std::string> get_seeds() const { return seeds; }
 
 	void set_hit_cap(const unsigned x) { hit_cap = x; }
 	void set_dbf_size(const unsigned x) { dbf_size = x; }
